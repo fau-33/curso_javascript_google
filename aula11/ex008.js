28f2a1daeb879f28dba63e5109bfa0695b1da460
Flávio Félix <fau_felix30@hotmail.com>
2020-06-25 19:42:31 -0300

    Feito a aula 11 e o desafio ex011

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 80
+console.log(`A velocidade de seu carro é ${vel} km/h`)
+if(vel > 60){//Condição simples 
+  console.log(`Você ultrapassou a velocidade permitida, Multado`)
+}
+console.log(`Dirija sempre usando o cinto de segurança`)
