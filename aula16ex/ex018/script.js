0a211ca22a0aff798ca2e6c0be93a508f7a5104c
Flávio Félix <fau_felix30@hotmail.com>
2020-07-21 16:56:40 -0300

    Feito a aula sobre funções e o começo do exercício parte 7

---
+++ aula16ex/ex018/script.js
@@ -0,0 +1,30 @@
+let num = document.getSelection('input#fnum')
+let lista = document.getSelection('select#flista')
+let res = document.getSelection('div#res')
+let valores = []
+
+function isNumero(n){
+  if(Number(n) >= 1 && Number(n) <= 100){
+    return true
+  }else{
+    return false
+  }
+}
+
+function inLista(n, 1){
+  if(l.indexOf(Number(n)) != -1){
+    return true
+  }else{
+    return false
+  }
+}
+
+
+function adicionar(){
+  if(isNumero(num.value) && !inLista(num.value, valores)){
+    window.alert(`Tudo OK`)
+  }else{
+    window.alert(`Valor inválido e não encontrado na lista.`)
+  }
+}
+
